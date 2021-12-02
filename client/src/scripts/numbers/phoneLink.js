6309c8880837a44b610b9f370f48912f35a156bb
Kirill Alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2021-12-03 01:42:35 +0300

    fix: header styles

--- client/src/scripts/numbers/phoneLink.js
+++ client/src/scripts/numbers/phoneLink.js
@@ -1,8 +1,8 @@
 export const rewriteNumber = () => {
-	const phones = document.querySelectorAll('[data-tel="phone"]')
+	const phones = document.querySelectorAll('[data-phone="href"]')
 
 	phones.forEach($phone => {
-		const $number = $phone.querySelector('[data-tel="number"]')
+		const $number = $phone.querySelector('[data-phone="number"]')
 		const correnctLink = `tel:${$number.textContent.replace(/[^\d]/g, '')}`
 
 		$phone.href = correnctLink
