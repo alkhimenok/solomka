fa2e989082dd6e9a8f4703fef3294eb7125fccd8
Kirill Alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2021-12-06 14:09:22 +0300

    add: slider

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,15 @@ import { navs, handlerNavMove } from './scripts/pageMove/navMove'
 import { applications, moveToSendSection } from './scripts/pageMove/toSendSection'
 import { $burgerIcon, toggleShowMenu } from './scripts/burger'
 import { $sendBtn, showModalSuccess } from './scripts/UI/modal'
-import { $sliderNav, addHandlerCliderClicks } from './scripts/slider'
+import {
+	$slider,
+	$sliderNav,
+	setPositionTouchStart,
+	setPositionTouchEnd,
+	addHandlerCliderTouch,
+	addHandlerCliderClicks,
+	setPoginationSize,
+} from './scripts/slider'
 
 const start = () => {
 	try {
@@ -20,11 +28,15 @@ const start = () => {
 		setMarginForHeader()
 		setPhoneMask()
 		rewriteCallLinks()
+		setPoginationSize()
 
 		navs.forEach($nav => $nav.addEventListener('click', handlerNavMove))
 		applications.forEach($application => $application.addEventListener('click', moveToSendSection))
 		$burgerIcon.addEventListener('click', toggleShowMenu)
 		$sendBtn.addEventListener('click', showModalSuccess)
+		$slider.addEventListener('touchstart', setPositionTouchStart)
+		$slider.addEventListener('touchmove', setPositionTouchEnd)
+		$slider.addEventListener('touchend', addHandlerCliderTouch)
 		$sliderNav.addEventListener('click', addHandlerCliderClicks)
 	} catch (error) {
 		console.error(error)
---
+++ client/src/scripts/slider.js
@@ -0,0 +1,58 @@
+import { $body } from './indent'
+
+const $products = document.querySelector('#products')
+
+export const $slider = $products.querySelector('#slider')
+export const $sliderNav = $products.querySelector('#sliderNav')
+
+const $arrowNext = $sliderNav.querySelector(`#arrowNext`)
+const $arrowPrev = $sliderNav.querySelector('#arrowPrev')
+const $poginationThumb = $products.querySelector('#poginationThumb')
+const $currentSlide = $products.querySelector('#currentSlide')
+const $totalSlides = $products.querySelector('#totalSlides')
+
+const slides = $slider.children
+
+const MIN_SLIDES = 0
+const MAX_SLIDES = slides.length
+
+let touchStart
+let touchEnd
+let curretSlide = 0
+
+export const setPoginationSize = () => {
+	$poginationThumb.style.width = `${100 / MAX_SLIDES}%`
+	$totalSlides.textContent = String(MAX_SLIDES).padStart(2, 0)
+}
+
+export const setPositionTouchStart = e => (touchStart = e.touches[0].clientX)
+export const setPositionTouchEnd = e => (touchEnd = e.touches[0].clientX)
+export const addHandlerCliderTouch = () => moneSlide(touchStart > touchEnd ? $arrowNext : $arrowPrev)
+export const addHandlerCliderClicks = e => (e.target.classList.contains('products__arrow') ? moneSlide(e.target) : null)
+
+const moneSlide = arrow => {
+	if (arrow.classList.contains('_disable')) return
+
+	const BODY_WIDTH = $body.clientWidth
+	const SLIDE_WIDTH = slides[0]?.clientWidth
+	const SWIPE_WIDTH = BODY_WIDTH <= 1180 ? SLIDE_WIDTH : SLIDE_WIDTH * 2
+
+	arrow.classList.contains('_arrow-next') ? curretSlide++ : curretSlide--
+
+	$currentSlide.textContent = String(curretSlide + 1).padStart(2, 0)
+	$poginationThumb.style.transform = `translateX(${$poginationThumb.clientWidth * curretSlide}px)`
+	$slider.style.transform = `translateX(-${SWIPE_WIDTH * curretSlide}px)`
+
+	checkSliderBoundaries()
+}
+
+const checkSliderBoundaries = () => {
+	if (curretSlide === MIN_SLIDES) {
+		$arrowPrev.classList.add('_disable')
+	} else if (curretSlide === MAX_SLIDES - 1) {
+		$arrowNext.classList.add('_disable')
+	} else {
+		$arrowPrev.classList.remove('_disable')
+		$arrowNext.classList.remove('_disable')
+	}
+}
