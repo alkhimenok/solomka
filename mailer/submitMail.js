2e9e208ab0302f2742448aaebbdc4ef3dacb1d4f
Kirill Alkhimenok <alkhimenok.kirill.leonidovich@gmail.com>
2022-03-20 01:35:30 +0300

    Update submitMail.js

--- mailer/submitMail.js
+++ mailer/submitMail.js
@@ -26,20 +26,20 @@ const getMailOptions = data => {
 		subject: 'Пользователь хочет получить вошу консультацию',
 		html: `
 			<div style="padding: 10px; font-family: Verdana, Geneva, Tahoma, sans-serif">
-        <div style="margin: 25px 0">
-          <h3 style="display: block; margin: 0 15px 0 0; color: #000000">Имя пользователя:</h3>
-          <p style="margin: 5px 0px; font-size: 22px; letter-spacing: 1px; color: #464646; text-transform: capitalize; background-color: #fafafa;">${name}</p>
-        </div>
-        <div style="margin: 25px 0">
-          <h3 style="display: block; margin: 0 15px 0 0; color: #000000">Номер телефона:</h3>
-          <a href="${callLink}" style="text-decoration: none">
-            <p style="margin: 5px 0px; font-size: 22px; letter-spacing: 1px; color: #464646; text-transform: uppercase; background-color: #fafafa;">${phone}</p>
-          </a>
-        </div>
-        <div style="margin: 25px 0">
-          <h3 style="display: block; margin: 0 15px 0 0; color: #000000">Заявка была отправлена:</h3>
-          <p style="margin: 5px 0px; font-size: 22px; letter-spacing: 1px; color: #464646; text-transform: uppercase; background-color: #fafafa;">${date}</p>
-        </div>
+				<div style="margin: 25px 0">
+				  <h3 style="display: block; margin: 0 15px 0 0; color: #000000">Имя пользователя:</h3>
+				  <p style="margin: 5px 0px; font-size: 22px; letter-spacing: 1px; color: #464646; text-transform: capitalize; background-color: #fafafa;">${name}</p>
+				</div>
+				<div style="margin: 25px 0">
+				  <h3 style="display: block; margin: 0 15px 0 0; color: #000000">Номер телефона:</h3>
+				  <a href="${callLink}" style="text-decoration: none">
+				    <p style="margin: 5px 0px; font-size: 22px; letter-spacing: 1px; color: #464646; text-transform: uppercase; background-color: #fafafa;">${phone}</p>
+				  </a>
+				</div>
+				<div style="margin: 25px 0">
+				  <h3 style="display: block; margin: 0 15px 0 0; color: #000000">Заявка была отправлена:</h3>
+				  <p style="margin: 5px 0px; font-size: 22px; letter-spacing: 1px; color: #464646; text-transform: uppercase; background-color: #fafafa;">${date}</p>
+				</div>
 			</div>
 		`,
 	}
